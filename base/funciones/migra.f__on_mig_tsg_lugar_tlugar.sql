09f51a84ac79a251fb87ac31b77789e71bb87fab
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-06-18 18:58:00 -0400

    -#169 Titulo: migracion de la tabla lugar Duracion: 4 horas Descripcion: se realizo la migracion de la tabla tsg_lugar de endesis a pxp

---
+++ base/funciones/migra.f__on_mig_tsg_lugar_tlugar.sql
@@ -0,0 +1,165 @@
+CREATE OR REPLACE FUNCTION migra.f__on_trig_tsg_lugar_tlugar (
+  v_operacion varchar,
+  p_id_lugar integer,
+  p_id_lugar_fk integer,
+  p_codigo varchar,
+  p_codigo_largo varchar,
+  p_estado_reg varchar,
+  p_fecha_mod timestamp,
+  p_fecha_reg timestamp,
+  p_id_usuario_mod integer,
+  p_id_usuario_reg integer,
+  p_nombre varchar,
+  p_sw_impuesto varchar,
+  p_sw_municipio varchar,
+  p_tipo varchar
+)
+RETURNS text AS
+$body$
+/*
+						Function:  Para migracion de la tabla param.tgestion
+						Fecha Creacion:  June 7, 2013, 4:01 pm
+						Autor: autogenerado (ADMINISTRADOR DEL SISTEMA )
+						
+						*/
+						
+						DECLARE
+						
+						v_codigos 			record;
+                        v_codigo_largo		varchar;
+                        v_tamanio			int4;
+						
+						BEGIN
+						
+						    if(v_operacion = 'INSERT') THEN
+						
+						          INSERT INTO 
+						            PARAM.tlugar (
+						id_lugar,
+						id_lugar_fk,
+						codigo,
+						codigo_largo,
+						estado_reg,
+						fecha_mod,
+						fecha_reg,
+						id_usuario_mod,
+						id_usuario_reg,
+						nombre,
+						sw_impuesto,
+						sw_municipio,
+						tipo)
+				VALUES (
+						p_id_lugar,
+						p_id_lugar_fk,
+						p_codigo,
+						p_codigo_largo,
+						p_estado_reg,
+						p_fecha_mod,
+						p_fecha_reg,
+						p_id_usuario_mod,
+						p_id_usuario_reg,
+						p_nombre,
+						p_sw_impuesto,
+						p_sw_municipio,
+						p_tipo);
+						
+						create temp table codigos(
+                        	  id_lugar int4,
+                              id_lugar_fk int4,
+                              codigo varchar
+                        ) on commit drop;
+
+						insert into codigos(
+						WITH RECURSIVE otro(id_lugar, id_lugar_fk, codigo)AS(		
+                        SELECT l.id_lugar, l.id_lugar_fk, l.codigo
+                            FROM param.tlugar l
+                            WHERE l.id_lugar=p_id_lugar and l.id_lugar_fk!=p_id_lugar
+                        UNION ALL 
+                            SELECT l.id_lugar, l.id_lugar_fk, l.codigo
+                            FROM param.tlugar l, otro o
+                            WHERE l.id_lugar=o.id_lugar_fk
+                        )select * from otro order by id_lugar ASC);
+                        
+                        select count(id_lugar) into v_tamanio from codigos;
+                        if (v_tamanio!=0) then
+                            FOR v_codigos in select * from codigos                        
+                            LOOP
+                                v_codigo_largo:=pxp.concatvarchar(v_codigo_largo,v_codigos.codigo);
+                            END LOOP;                       
+                    		
+                            UPDATE PARAM.tlugar SET 
+                             codigo_largo=v_codigo_largo
+                             WHERE id_lugar=p_id_lugar;						
+
+						end if;						       
+							    ELSEIF  v_operacion = 'UPDATE' THEN
+						               UPDATE 
+						                  PARAM.tlugar  
+						                SET						 id_lugar_fk=p_id_lugar_fk
+						 ,codigo=p_codigo
+						 ,codigo_largo=p_codigo_largo
+						 ,estado_reg=p_estado_reg
+						 ,fecha_mod=p_fecha_mod
+						 ,fecha_reg=p_fecha_reg
+						 ,id_usuario_mod=p_id_usuario_mod
+						 ,id_usuario_reg=p_id_usuario_reg
+						 ,nombre=p_nombre
+						 ,sw_impuesto=p_sw_impuesto
+						 ,sw_municipio=p_sw_municipio
+						 ,tipo=p_tipo
+						 WHERE id_lugar=p_id_lugar;
+                         
+                         create temp table codigos(
+                        	  id_lugar int4,
+                              id_lugar_fk int4,
+                              codigo varchar
+                        ) on commit drop;
+
+						insert into codigos(
+						WITH RECURSIVE otro(id_lugar, id_lugar_fk, codigo)AS(		
+                        SELECT l.id_lugar, l.id_lugar_fk, l.codigo
+                            FROM param.tlugar l
+                            WHERE l.id_lugar=p_id_lugar and l.id_lugar_fk!=p_id_lugar
+                        UNION ALL 
+                            SELECT l.id_lugar, l.id_lugar_fk, l.codigo
+                            FROM param.tlugar l, otro o
+                            WHERE l.id_lugar=o.id_lugar_fk
+                        )select * from otro order by id_lugar ASC);
+                        
+                        select count(id_lugar) into v_tamanio from codigos;
+                        if (v_tamanio!=0) then
+                            FOR v_codigos in select * from codigos                        
+                            LOOP
+                                v_codigo_largo:=pxp.concatvarchar(v_codigo_largo,v_codigos.codigo);
+                            END LOOP;                       
+                    		
+                            UPDATE PARAM.tlugar SET 
+                             codigo_largo=v_codigo_largo
+                             WHERE id_lugar=p_id_lugar;						
+
+						end if;	
+
+						       
+						       ELSEIF  v_operacion = 'DELETE' THEN
+						       
+						         DELETE FROM 
+						              PARAM.tlugar
+ 
+						              						 WHERE id_lugar=p_id_lugar;
+
+						       
+						       END IF;  
+						  
+						 return 'true';
+						
+						-- statements;
+						--EXCEPTION
+						--WHEN exception_name THEN
+						--  statements;
+						END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100;
---
+++ base_origen/funciones_de_migracion/tsg_lugar_tlugar/migracion.f_trans_tsg_lugar_tlugar.sql
@@ -0,0 +1,118 @@
+CREATE OR REPLACE FUNCTION migracion.f_trans_tsg_lugar_tlugar (
+  v_operacion varchar,
+  p_nombre varchar,
+  p_id_lugar integer,
+  p_codigo varchar,
+  p_fk_id_lugar integer,
+  p_nivel integer,
+  p_prioridad_kard varchar,
+  p_sw_impuesto varchar,
+  p_sw_municipio varchar
+)
+RETURNS varchar [] AS
+$body$
+DECLARE
+			 
+			g_registros record;
+			v_consulta varchar;
+			v_res_cone  varchar;
+			v_cadena_cnx varchar;
+			v_cadena_con varchar;
+			resp boolean;
+			v_resp varchar;
+			v_respuesta varchar[];
+			
+			g_registros_resp record;
+			v_id_lugar int4;
+			v_id_lugar_fk int4;
+			v_codigo varchar;
+			v_codigo_largo varchar;
+			v_estado_reg varchar;
+			v_fecha_mod timestamp;
+			v_fecha_reg timestamp;
+			v_id_usuario_mod int4;
+			v_id_usuario_reg int4;
+			v_nombre varchar;
+			v_sw_impuesto varchar;
+			v_sw_municipio varchar;
+			v_tipo varchar;
+BEGIN
+			
+			
+			          --funcion para obtener cadena de conexion
+			          v_cadena_cnx =  migracion.f_obtener_cadena_con_dblink();
+			          
+			          
+			           ---------------------------------------
+			           --previamente se tranforman los datos  (descomentar)
+			           ---------------------------------------
+
+			           
+			v_id_lugar=p_id_lugar::int4;
+			v_id_lugar_fk=p_fk_id_lugar::int4;
+			v_codigo=convert(p_codigo::varchar, 'LATIN1', 'UTF8');
+			v_codigo_largo=NULL::varchar;
+			v_estado_reg=convert('activo'::varchar, 'LATIN1', 'UTF8');
+			v_fecha_mod=NULL::timestamp;
+			v_fecha_reg=now()::timestamp;
+			v_id_usuario_mod=NULL::int4;
+			v_id_usuario_reg=1::int4;
+			v_nombre=convert(p_nombre::varchar, 'LATIN1', 'UTF8');
+			v_sw_impuesto=convert(p_sw_impuesto::varchar, 'LATIN1', 'UTF8');
+			v_sw_municipio=convert(p_sw_municipio::varchar, 'LATIN1', 'UTF8');
+			if p_nivel=0 then
+                v_tipo='pais'::varchar;                
+				v_id_lugar_fk=NULL::int4;
+            elsif p_nivel=1 then
+            	v_tipo='departamento'::varchar;
+    		elsif p_nivel=2 then
+            	v_tipo='provincia'::varchar;
+            elsif p_nivel=3 then
+            	v_tipo='localidad'::varchar;
+            else
+            	v_tipo='barrio'::varchar;
+            end if;
+			    --cadena para la llamada a la funcion de insercion en la base de datos destino
+			      
+			        
+			          v_consulta = 'select migra.f__on_trig_tsg_lugar_tlugar (
+			               '''||v_operacion::varchar||''','||COALESCE(v_id_lugar::varchar,'NULL')||','||COALESCE(v_id_lugar_fk::varchar,'NULL')||','||COALESCE(''''||v_codigo::varchar||'''','NULL')||','||COALESCE(''''||v_codigo_largo::varchar||'''','NULL')||','||COALESCE(''''||v_estado_reg::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_mod::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_reg::varchar||'''','NULL')||','||COALESCE(v_id_usuario_mod::varchar,'NULL')||','||COALESCE(v_id_usuario_reg::varchar,'NULL')||','||COALESCE(''''||v_nombre::varchar||'''','NULL')||','||COALESCE(''''||v_sw_impuesto::varchar||'''','NULL')||','||COALESCE(''''||v_sw_municipio::varchar||'''','NULL')||','||COALESCE(''''||v_tipo::varchar||'''','NULL')||')';
+			          --probar la conexion con dblink
+	
+					   --probar la conexion con dblink
+			          v_resp =  (SELECT dblink_connect(v_cadena_cnx));
+			            
+			             IF(v_resp!='OK') THEN
+			            
+			             	--modificar bandera de fallo  
+			                 raise exception 'FALLA CONEXION A LA BASE DE DATOS CON DBLINK';
+			                 
+			             ELSE
+					  
+			         
+			               PERFORM * FROM dblink(v_consulta,true) AS ( xx varchar);
+			                v_res_cone=(select dblink_disconnect());
+			             END IF;
+			            
+			            v_respuesta[1]='TRUE';
+                       
+			           RETURN v_respuesta;
+			EXCEPTION
+			   WHEN others THEN
+			   
+			    v_res_cone=(select dblink_disconnect());
+			     v_respuesta[1]='FALSE';
+                 v_respuesta[2]=SQLERRM;
+                 v_respuesta[3]=SQLSTATE;
+                 v_respuesta[4]=v_consulta;
+                 
+    
+                 
+                 RETURN v_respuesta;
+			
+			END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER;
