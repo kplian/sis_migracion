9c0fbf4acc4fad360cd830d48cd1811d2081a3cc
dora <dcastro@boa.bo>
2014-02-19 19:49:34 -0400

    anadiendo llave foranea en concepto_ids

--- base/dependencies000001.sql
+++ base/dependencies000001.sql
@@ -1,6 +1,12 @@
 
    
 /***********************************I-DEP-RAC-MIGRA-0-31/12/2012*****************************************/
+ALTER TABLE migra.tconcepto_ids
+  ADD CONSTRAINT tconcepto_ids_fk FOREIGN KEY (id_concepto_ingas_pxp)
+    REFERENCES param.tconcepto_ingas(id_concepto_ingas)
+    MATCH FULL
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    NOT DEFERRABLE;
 
-
-/***********************************F-DEP-RAC-MIGRA-0-04/01/2013*****************************************/
+/***********************************F-DEP-RAC-MIGRA-0-31/12/2013*****************************************/
