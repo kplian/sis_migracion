7e3875ccb2c07764c0e07efb967867a972a42e48
dora <dcastro@boa.bo>
2014-03-11 18:11:40 -0400

    solucionado bug de funcion de preingresos a activos fijos

--- base_origen/funciones_basicas/migracion.f_af_genera_registro_af.sql
+++ base_origen/funciones_basicas/migracion.f_af_genera_registro_af.sql
@@ -1,3 +1,5 @@
+--------------- SQL ---------------
+
 CREATE OR REPLACE FUNCTION migracion.f_af_genera_registro_af (
   p_id_usuario integer,
   p_id_preingreso integer
@@ -5,9 +7,16 @@ CREATE OR REPLACE FUNCTION migracion.f_af_genera_registro_af (
 RETURNS varchar AS
 $body$
 /*
-Autor: RCM
+Autor: RCM  (KPLIAN)
 Fecha: 06/10/2013
 Descripción: genera un registro de activo fijo a partir de pxp con una conexión dblink
+---------
+FECHA 11/3/2014
+AUTOR: RAC (KPLIAN)
+Descripcion:   SE corrgie la consulta de isnercion  que devolvial elementos que noestaba en el preingreso,
+               se aumentar un for para la cantidad de items introducidos
+
+
 */
 DECLARE
 
@@ -21,7 +30,8 @@ DECLARE
     v_id_presupuesto integer;
     v_id_deposito integer;
     v_result	varchar;
-    v_lim	integer;
+    
+    v_i			integer;
 
 BEGIN
 
@@ -36,21 +46,39 @@ BEGIN
     end if;
 
 	--Valor por defecto para la descripción de los activos fijos
-    v_desc = '[REGISTRADO AUTOMATICAMENTE POR SISTEMA DE COMPRAS, MODIFIQUE Y COMPLETE LA INFORMACION]';
+    v_desc = '[REGISTRADO POR SISTEMA DE COMPRAS]';
 
     v_sql = 'select
-    		sdet.descripcion, cot.fecha_adju, pdet.precio_compra, ping.id_moneda,
-            pdet.cantidad_det, pdet.id_depto, pdet.id_clasificacion,pdet.observaciones,
-            cot.numero_oc, cot.id_cotizacion, pdet.id_cotizacion_det, sol.id_solicitud,
-            cc.id_centro_costo,cc.id_ep,cc.id_uo,cc.id_gestion
+    		   sdet.descripcion, 
+               cot.fecha_adju, 
+               pdet.precio_compra, 
+               ping.id_moneda,
+               pdet.cantidad_det,
+               pdet.id_depto, 
+               pdet.id_clasificacion,
+               pdet.observaciones,
+               cot.numero_oc, 
+               cot.id_cotizacion, 
+               pdet.id_cotizacion_det, 
+               sol.id_solicitud,
+               cc.id_centro_costo,
+               cc.id_ep,
+               cc.id_uo,
+               cc.id_gestion,
+               cg.desc_ingas
     		from alm.tpreingreso ping
             inner join alm.tpreingreso_det pdet on pdet.id_preingreso = ping.id_preingreso
-            inner join adq.tcotizacion cot on cot.id_cotizacion = ping.id_cotizacion
+            inner join adq.tcotizacion_det  cd on cd.id_cotizacion_det = pdet.id_cotizacion_det
+            inner join adq.tsolicitud_det sdet on sdet.id_solicitud_det =cd.id_solicitud_det
+            inner join param.tconcepto_ingas cg on cg.id_concepto_ingas = sdet.id_concepto_ingas
+            
+            inner join adq.tcotizacion cot on cot.id_cotizacion = cd.id_cotizacion
             inner join adq.tproceso_compra pro on pro.id_proceso_compra = cot.id_proceso_compra
             inner join adq.tsolicitud sol on sol.id_solicitud = pro.id_solicitud
-            inner join adq.tsolicitud_det sdet on sdet.id_solicitud = sol.id_solicitud
+            
+          
             inner join param.tcentro_costo cc on cc.id_centro_costo = sdet.id_centro_costo
-            where ping.id_preingreso = '||p_id_preingreso;
+            where ping.id_preingreso  = '||p_id_preingreso;
             
     
     for v_rec in (select * from dblink(v_sql,true) as (descripcion TEXT,
@@ -68,8 +96,9 @@ BEGIN
                                                             id_centro_costo INTEGER,
                                                             id_ep INTEGER,
                                                             id_uo INTEGER,
-                                                            id_gestion INTEGER)) loop
-    	--Obtener la vida util
+                                                            id_gestion INTEGER,
+                                                            desc_ingas VARCHAR)) loop
+    	--Obtener la vida útil
         select vida_util
         into v_vida_util
         from actif.taf_clasificacion
@@ -91,88 +120,84 @@ BEGIN
         from actif.taf_deposito
         where id_depto_af = v_rec.id_depto;
         
-        --Registro del activo fijo en función de la cantidad
+        FOR v_i in 1..v_rec.cantidad_det::integer LOOP 
         
-        v_lim = floor(v_rec.cantidad_det)::integer;
-        
-        for i in 1..v_lim loop
-        
+            --Registro del activo fijo en función de la cantidad
             v_result = actif.f_taf_activo_fijo_iud(
-                p_id_usuario::integer,
-                '192.168.225.0'::varchar,
-                '99:99:99:99:99:99'::text,
-                'AF_AF_INS'::varchar,
-                NULL::varchar,
-                v_rec.id_ep::integer,
-                NULL::integer,
-                NULL::varchar,
-                v_rec.descripcion::varchar,
-                v_desc::varchar, --10
-                v_vida_util::integer,
-                v_vida_util::integer,
-                (v_vida_util/100)::numeric,
-                NULL::date,  --fecha_ult_deprec
-                0::numeric,
-                0::numeric,
-                0::numeric,
-                'no'::varchar,
-                1::numeric,
-                v_rec.fecha_adju::date, --20
-                v_rec.precio_compra::numeric,
-                v_rec.precio_compra::numeric,
-                v_rec.precio_compra::numeric,
-                'no'::varchar,
-                NULL::varchar,
-                NULL::date,
-                now()::date,
-                NULL::bytea,
-                NULL::varchar,
-                1::numeric,--30
-                'registrado'::varchar,
-                v_rec.observaciones::varchar,
-                NULL::integer, 
-                NULL::integer, 
-                v_rec.id_moneda::integer, 
-                v_rec.id_moneda::integer, 
-                NULL::integer, 
-                NULL::date, 
-                NULL::varchar,
-                v_rec.numero_oc::varchar, --40
-                1::integer,
-                v_rec.precio_compra::numeric, 
-                v_rec.precio_compra::numeric, 
-                0::numeric, 
-                0::numeric, 
-                0::numeric, 
-                v_vida_util::integer, 
-                v_vida_util::integer, 
-                v_rec.id_depto::integer, 
-                v_rec.id_cotizacion::integer, --50
-                v_rec.id_cotizacion_det::integer, 
-                NULL::varchar,
-                v_id_presupuesto::integer,
-                NULL::integer,
-                v_rec.id_gestion::integer,
-                v_rec.id_solicitud::integer,
-                'no',--'si'::varchar,
-                NULL::integer,--(v_rec.cantidad_det-1)::integer,
-                v_id_deposito::integer,
-                'activo'::varchar, --60
-                'no'::varchar, 
-                v_rec.id_clasificacion::integer, 
-                NULL::varchar,
-                NULL::date,
-                v_rec.precio_compra::numeric --65
-            );
+                    p_id_usuario::integer,
+                    '192.168.225.0'::varchar,
+                    '99:99:99:99:99:99'::text,
+                    'AF_AF_INS'::varchar,
+                    NULL::varchar,
+                    v_rec.id_ep::integer,
+                    NULL::integer,
+                    NULL::varchar,
+                    (v_desc||'  '||v_rec.desc_ingas||' - '||v_rec.descripcion)::text,
+                    '[INTRODUCIR DESCRIPCION]'::varchar, --10
+                    v_vida_util::integer,
+                    v_vida_util::integer,
+                    (v_vida_util/100)::numeric,
+                    NULL::date,  --fecha_ult_deprec
+                    0::numeric,
+                    0::numeric,
+                    0::numeric,
+                    'no'::varchar,
+                    1::numeric,
+                    v_rec.fecha_adju::date, --20
+                    v_rec.precio_compra::numeric,
+                    v_rec.precio_compra::numeric,
+                    v_rec.precio_compra::numeric,
+                    'no'::varchar,
+                    NULL::varchar,
+                    NULL::date,
+                    now()::date,
+                    NULL::bytea,
+                    NULL::varchar,
+                    1::numeric,--30
+                    'registrado'::varchar,
+                    v_rec.observaciones::varchar,
+                    NULL::integer, 
+                    NULL::integer, 
+                    v_rec.id_moneda::integer, 
+                    v_rec.id_moneda::integer, 
+                    NULL::integer, 
+                    NULL::date, 
+                    NULL::varchar,
+                    v_rec.numero_oc::varchar, --40
+                    1::integer,
+                    v_rec.precio_compra::numeric, 
+                    v_rec.precio_compra::numeric, 
+                    0::numeric, 
+                    0::numeric, 
+                    0::numeric, 
+                    v_vida_util::integer, 
+                    v_vida_util::integer, 
+                    v_rec.id_depto::integer, 
+                    v_rec.id_cotizacion::integer, --50
+                    v_rec.id_cotizacion_det::integer, 
+                    NULL::varchar,
+                    v_id_presupuesto::integer,
+                    NULL::integer,
+                    v_rec.id_gestion::integer,
+                    v_rec.id_solicitud::integer,
+                    'no',--'si'::varchar,
+                    NULL::integer,--(v_rec.cantidad_det-1)::integer,
+                    v_id_deposito::integer,
+                    'activo'::varchar, --60
+                    'no'::varchar, 
+                    v_rec.id_clasificacion::integer, 
+                    NULL::varchar,
+                    NULL::date,
+                    v_rec.precio_compra::numeric --65
+                    );
+    	END LOOP;
 
-            if substr(v_result,1,1) = 'f' then
-                raise exception '%',v_result;
-            end if;
-            
-		end loop;
+        if substr(v_result,1,1) = 'f' then
+        	raise exception '%',v_result;
+        end if;
     
     end loop;
-
+    --raise exception 'fuck';
     return 'Hecho';
     
 
--- base_origen/path000001.sql
+++ base_origen/path000001.sql
@@ -28,3 +28,15 @@ ALTER TABLE sci.tct_comprobante
 --18-11-2013
 ALTER TABLE sci.tct_comprobante
   ADD CONSTRAINT chk_tct_comprobante__origen CHECK ((((((((((((((((((((((((((((origen)::text = 'alta_activo_fijo'::text) OR ((origen)::text = 'depreciacion_activo_fijo'::text)) OR ((origen)::text = 'devengado_diario'::text)) OR ((origen)::text = 'fv_facturacion_mesual'::text)) OR ((origen)::text = 'devengado_pago'::text)) OR ((origen)::text = 'devengado_reg'::text)) OR ((origen)::text = 'duplicado'::text)) OR ((origen)::text = 'finalizacion'::text)) OR ((origen)::text = 'planilla_devengado'::text)) OR ((origen)::text = 'planilla_pago'::text)) OR ((origen)::text = 'plan_pago_anticipo'::text)) OR ((origen)::text = 'plan_pago_devengado'::text)) OR ((origen)::text = 'plan_pago_pago'::text)) OR ((origen)::text = 'rendicion'::text)) OR ((origen)::text = 'reposicion'::text)) OR ((origen)::text = 'solicitud'::text)) OR ((origen)::text = 'sucursal'::text)) OR ((origen)::text = 'kp_planilla_diario_pre'::text)) OR ((origen)::text = 'actualizacion'::text)) OR ((origen)::text = 'kp_planilla_anticipo'::text)) OR ((origen)::text = 'activo_fijo'::text)) OR ((origen)::text = 'kp_planilla_diario_costo'::text)) OR (origen IS NULL)) OR ((origen)::text = 'cierre_apertura'::text)) OR ((origen)::text = 'kp_planilla_obligacion'::text)) OR ((origen)::text = 'comprobante_apertura'::text)) OR ((origen)::text = 'comprobante_apertura'::text));
+  
+
+
+--------------- SQL ---------------
+
+ALTER TABLE actif.taf_activo_fijo
+  ALTER COLUMN descripcion TYPE VARCHAR(1000);
+  
+  
+--FALTA EL ESCRIPT PARA ELIMNAR LAS FOREN KEYS DE LA TABLA DE ACTIVO FIJO PARA
+-- EL PREINGERSO DE ACTIVOS  
+  
